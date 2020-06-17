3b31b4a147074284ee91fb7c0e538bb6df50ff08
yyalexyy <44172655+ajuan3@users.noreply.github.com>
2020-06-17 14:47:08 -0400

    Add files via upload

---
+++ mysite/register/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render, redirect
+#from .forms import RegisterForm
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+
+# Create your views here.
+'''def register(response):
+	if response.method == "POST":
+		form = RegisterForm(response.POST)
+		if form.is_valid():
+			form.save()
+		return redirect("/home")
+	else:
+		form = RegisterForm()
+
+	return render(response, "register/register.html", {"form":form})
+'''
+
+def register(response):
+	form = UserCreationForm()
+	return render(response, "register/register.html", {"form":form})
